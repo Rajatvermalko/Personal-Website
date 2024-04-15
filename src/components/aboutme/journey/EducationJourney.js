685df0ea8c5b63d14ccc7d614951170fc8af04cf
Rajat Verma <104197270+Rajatvermalko@users.noreply.github.com>
2024-04-15 22:58:19 +0530

    Update EducationJourney.js

--- src/components/aboutme/journey/EducationJourney.js
+++ src/components/aboutme/journey/EducationJourney.js
@@ -35,55 +35,37 @@ function EducationJourney() {
           className="vertical-timeline-element--work"
           contentStyle={{ background: "rgb(78 22 112)", color: "#fbd9ad" }}
           contentArrowStyle={{ borderRight: "7px solid  rgb(78 22 112)" }}
-          date="2004 - 2014"
+          date="Till 2015"
           iconStyle={{ background: "rgb(78 22 112)", color: "#fbd9ad" }}
           icon={<SchoolIcon />}
         >
           <h4 className="vertical-timeline-element-title">
-            SECONDARY SCHOOL CERTIFICATE
+            Senior Secondary Certificate
           </h4>
           <h4 className="vertical-timeline-element-subtitle mt-2">
-            BOF High School, Gazipur
+             City Montessori School, Lucknow 
           </h4>
           <p>
-            Studied here from Class 1 to 10th as a part of secondary school
-            Certificate and gain GPA 5 in my SSC Examination.
+            Studied here till Class 12th as a part of secondary school
+            Certificate and gained 83% in Examination.
           </p>
         </VerticalTimelineElement>
         <VerticalTimelineElement
           className="vertical-timeline-element--work"
           contentStyle={{ background: "rgb(78 22 112)", color: "#fbd9ad" }}
           contentArrowStyle={{ borderRight: "7px solid  rgb(78 22 112)" }}
-          date="2014 - 2018"
+          date="2015 - 2019"
           iconStyle={{ background: "rgb(78 22 112)", color: "#fbd9ad" }}
           icon={<SchoolIcon />}
         >
           <h4 className="vertical-timeline-element-title">
-            DIPLOMA IN ENGINEERING
+            Bachelors of Technology
           </h4>
           <h4 className="vertical-timeline-element-subtitle mt-2">
-            Dhaka Polytechnic Institute, Dhaka
+            Lovely Professional University , Jalandhar, Punjab 
           </h4>
           <p>
-            Studied Diploma in Computer Technology from Dhaka Polytechnic
-            institute and gain CGPA 2.98.
-          </p>
-        </VerticalTimelineElement>
-        <VerticalTimelineElement
-          className="vertical-timeline-element--work"
-          contentStyle={{ background: "rgb(78 22 112)", color: "#fbd9ad" }}
-          contentArrowStyle={{ borderRight: "7px solid  rgb(78 22 112)" }}
-          date="2019 - 2023"
-          iconStyle={{ background: "rgb(78 22 112)", color: "#fbd9ad" }}
-          icon={<SchoolIcon />}
-        >
-          <h4 className="vertical-timeline-element-title">GRADUATION</h4>
-          <h4 className="vertical-timeline-element-subtitle mt-2">
-            Sonargaon University, Dhaka
-          </h4>
-          <p>
-            Studied Bachelor of Science degree in Computer Science and
-            Engineering field from Sonargaon University and gain CGPA 3.44.
+            Studied Engineering in Civil and gained CGPA 7.01
           </p>
         </VerticalTimelineElement>
 
